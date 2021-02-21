0af8de52fa79a9145c1ed46be86a64ec9d8bcb34
Marika <MarikaHasse@googlemail.com>
2021-02-21 16:19:07 +0100

    creat public routes

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 
+//make any file inside public folder accessable without creating routes for each file
+app.use(express.static(__dirname + '/public'));
 
+app.get('/',(req,res)=>{
+   // console.log(__dirname)
+    res.sendFile(__dirname + '/views/index.html') 
+    // res.send('hello');
+})
 
 app.listen(port,()=>{
     console.log(`app is running on port: ${port}`);

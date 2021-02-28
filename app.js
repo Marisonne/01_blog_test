1bfdffa0ea395f23b636fd36ecb7c8f23f582084
Marika <MarikaHasse@googlemail.com>
2021-02-28 21:05:01 +0100

    save temp data

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.set ('views', __dirname + '/views')
 //make any file inside public folder accessable without creating routes for each file
 app.use(express.static(__dirname + '/public'));
 
+// set middleware to be able to get POSTed data
+// If extended is false, you can NOT post 'nested Objects'
+app.use(express.urlencoded({express: true}));
+app.use(express.json());
+
+
 
 app.get('/',(req,res)=>{
    // console.log(__dirname)
@@ -28,34 +34,54 @@ app.get('/about',(req,res)=>{
     res.render('about')
 })
 
-app.get('/marketing',(req,res)=>{
-    res.render('marketing')
-})
-
 
 //create a comments array to save the comments inside 
 
 const commentsArr = [
    {  
     name: 'Faelan',
-    commentDate : new Date('2021, 2, 18, 10'),
-    comment :" I'm an elf with long ears"
+
+    commentDate: new Date(2021, 2, 16, 10), 
+    comment:" I'm an elf with long ears"
 },
 {  
     name: 'Maèl',
-    commentDate : new Date('2021, 2, 17, 10'),
+    commentDate : new Date(2021, 2, 17, 07),
     comment :" I'm an elf with magic"
 },
 {  
     name: 'Mirel',
-    commentDate : new Date('2021, 2, 16, 10'),
-    comment :" I'm an elf. Do you like me?"
+    commentDate: new Date(2021, 2, 18, 09),
+    comment:" I'm an elf. Do you like me?"
 }
 ]
 
 
 app.get('/blog',(req,res)=>{
     res.render('blog',{commentsArr})
+})
+
+
+app.post('/blog',(req,res)=>{
+    console.log(req.body)
+    // push new data to commentsArr  
+    
+    commentsArr.push({      
+        name:req.body.name,
+        commentDate: new Date(),   // Date.now() 
+        comment: req.body.comment,
+        email: req.body.email
+    })
+
+    //  let newData = {
+    //      name:req.body.name,
+    //      commentDate: new Date(),
+    //      comment: req.body.comment,
+    //      email: req.body.email
+    //  }
+    //  commentsArr.unshift(newData);
+
+    res.render('blog', {commentsArr})
 })
 
 app.get('/contact',(req,res)=>{
@@ -66,3 +92,6 @@ app.get('/contact',(req,res)=>{
 app.listen(port,()=>{
     console.log(`app is running on port: ${port}`);
 })
+
+
+// commentDate : new Date(2021, 2, 17, 10).toLocaleTimeString(),
